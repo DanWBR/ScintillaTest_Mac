f600f362d12fbe123288419e91843315bd918997
DanWBR <danielwag@gmail.com>
2019-04-04 17:40:40 -0400

    first commit

---
+++ ScintillaTest/ViewController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace ScintillaTest
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var view = new ScintillaBinder.ScintillaView();
+            view.Bounds = new CoreGraphics.CGRect(0, 0, 500, 500);
+            view.WantsLayer = true;
+            view.Layer.BackgroundColor = NSColor.Red.CGColor;
+            this.View = view;
+
+        }
+
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
